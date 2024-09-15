60aa8a5e07fe0c09c667b519fbafe08b4182f034
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-09-15 17:54:10 +0700

    create file

---
+++ backend/bookCar/src/main/java/com/freelance/bookCar/BookCarApplication.java
@@ -0,0 +1,13 @@
+package com.freelance.bookCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCarApplication.class, args);
+	}
+
+}
