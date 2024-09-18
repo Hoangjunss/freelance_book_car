f5661acce49e96a81c232bd16efb090c38dfce56
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2024-09-18 13:04:36 +0700

    Bổ sung booking

---
+++ backend/bookCar/src/main/java/com/freelance/bookCar/controller/booking/BookingController.java
@@ -0,0 +1,38 @@
+package com.freelance.bookCar.controller.booking;
+
+import com.freelance.bookCar.dto.ApiResponse;
+import com.freelance.bookCar.dto.request.booking.CreateBookingRequest;
+import com.freelance.bookCar.dto.request.booking.UpdateBookingRequest;
+import com.freelance.bookCar.dto.response.product.booking.CreateBookingResponse;
+import com.freelance.bookCar.dto.response.product.booking.GetBookingResponse;
+import com.freelance.bookCar.dto.response.product.booking.UpdateBookingResponse;
+import com.freelance.bookCar.services.booking.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/booking")
+public class BookingController {
+    @Autowired
+    private BookingService bookingService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<CreateBookingResponse>> create(@RequestBody CreateBookingRequest createBookingRequest){
+        CreateBookingResponse createBookingResponse=bookingService.create(createBookingRequest);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Booking created successfully", createBookingResponse));
+    }
+
+    @PatchMapping()
+    public ResponseEntity<ApiResponse<UpdateBookingResponse>> update(@RequestBody UpdateBookingRequest updateBookingRequest){
+        UpdateBookingResponse updateBookingResponse =  bookingService.update(updateBookingRequest);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Booking updated successfully", updateBookingResponse));
+    }
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse<GetBookingResponse>> getById(@RequestParam Integer id){
+        GetBookingResponse GetBookingResponse = bookingService.findById(id);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Booking retrieved successfully", GetBookingResponse));
+    }
+
+}
