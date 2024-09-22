068d0b95dfff1141b2d23d4bbe1ae020ba8f4316
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2024-09-22 14:12:20 +0700

    khanh

--- frontend/Angular/src/app/app.routes.ts
+++ frontend/Angular/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { LocationListComponent } from './location-list/location-list.component';
-import { LoginComponent } from './login/login.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LocationDetailComponent } from './location-detail/location-detail.component';
+import { LocationListComponent } from './components/user/location-list/location-list.component';
+import { NavbarComponent } from './components/user/navbar/navbar.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { LocationDetailComponent } from './components/user/location-detail/location-detail.component';
 
 
 export const routes: Routes = [ 
