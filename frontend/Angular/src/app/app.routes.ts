d8185186cfbcb0af0f140bbdc68832cad3554125
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2024-09-24 17:29:26 +0700

    Giao diện Login/Register

--- frontend/Angular/src/app/app.routes.ts
+++ frontend/Angular/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NavbarComponent } from './components/user/navbar/navbar.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { LocationDetailComponent } from './components/user/location-detail/location-detail.component';
 import { BookingComponent } from './components/user/booking/booking.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 
 export const routes: Routes = [ 
@@ -16,10 +17,19 @@ export const routes: Routes = [
         path: 'home',
         component: NavbarComponent
     },
-    { 
-        path: '',
-        component: LoginComponent 
-    },
+    {
+        path: 'auth',
+        children: [
+          {
+            path: 'login',
+            component: LoginComponent
+          },
+          {
+            path: 'register',
+            component: RegisterComponent
+          }
+        ]
+      },
     {
         path:'location-detail',
         component: LocationDetailComponent
--- frontend/Angular/src/app/components/auth/register/register.component.ts
+++ frontend/Angular/src/app/components/auth/register/register.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-register',
   standalone: true,
   imports: [CommonModule, RouterLink],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
 })
-export class LoginComponent {
-  
+export class RegisterComponent {
+
 }
