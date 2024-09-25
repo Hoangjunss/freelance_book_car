92664faf8ed9c9ac82ba89d377b9dbb6fc91e2b8
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-09-25 22:09:33 +0700

    update tour

--- backend/bookCar/src/main/java/com/freelance/bookCar/config/WebSecutiryConfiguration.java
+++ backend/bookCar/src/main/java/com/freelance/bookCar/config/WebSecutiryConfiguration.java
@@ -39,7 +39,7 @@ private JwtAuthenticationFilter jwtAuthenticationFilter;
                 .csrf(AbstractHttpConfigurer::disable)
 
                 // Configure các luồng truy cập
-                .authorizeHttpRequests((auth) -> auth.requestMatchers("/api/v1/user/signup","/api/v1/user/signin","/api/v1/user/refreshtoken","/hotel","/tour","tourism").permitAll()
+                .authorizeHttpRequests((auth) -> auth.requestMatchers("/api/v1/user/signup","/api/v1/user/signin","/api/v1/user/refreshtoken","/hotel","/tour","/tourism").permitAll()
                     // Xác thực tất cả các request
                         .anyRequest()
                         .authenticated()
