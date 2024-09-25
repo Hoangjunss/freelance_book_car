d615f3adf46e9a03ed37dee808e505eb7e171638
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-09-25 09:58:18 +0700

    add controller

---
+++ backend/bookCar/src/main/java/com/freelance/bookCar/controller/auth/AuthController.java
@@ -0,0 +1,40 @@
+package com.freelance.bookCar.controller.auth;
+
+import com.freelance.bookCar.dto.ApiResponse;
+import com.freelance.bookCar.dto.request.booking.CreateBookingRequest;
+import com.freelance.bookCar.dto.request.user.accountDTO.LoginRequest;
+import com.freelance.bookCar.dto.request.user.accountDTO.RefreshToken;
+import com.freelance.bookCar.dto.request.user.accountDTO.RegistrationRequest;
+import com.freelance.bookCar.dto.response.booking.CreateBookingResponse;
+import com.freelance.bookCar.dto.response.user.accountDTO.LoginResponse;
+import com.freelance.bookCar.dto.response.user.accountDTO.RegistrationResponse;
+import com.freelance.bookCar.services.user.accountservice.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AccountService accountService;
+    @PostMapping("/registration")
+    public ResponseEntity<ApiResponse<RegistrationResponse>> registration(@RequestBody RegistrationRequest registrationRequest){
+        RegistrationResponse registrationResponse=accountService.registration(registrationRequest);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Registration  successfully", registrationResponse));
+    }
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<LoginResponse>> login(@RequestBody LoginRequest loginRequest){
+        LoginResponse loginResponse=accountService.login(loginRequest);
+        return ResponseEntity.ok(new ApiResponse<>(true, "Login  successfully", loginResponse));
+    }
+    @PostMapping("/refreshToken")
+    public ResponseEntity<ApiResponse<LoginResponse>> refreshToken(@RequestBody RefreshToken refreshToken){
+        LoginResponse loginResponse=accountService.generateRefreshToken(refreshToken);
+        return ResponseEntity.ok(new ApiResponse<>(true, "RefreshToken  successfully", loginResponse));
+    }
+
+}
