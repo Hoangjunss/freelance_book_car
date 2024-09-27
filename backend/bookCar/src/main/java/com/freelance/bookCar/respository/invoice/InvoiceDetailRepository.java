6d85725092b9316cfe23bf2b28461d1581640807
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2024-09-27 10:58:11 +0700

    Viết truy vấn

--- backend/bookCar/src/main/java/com/freelance/bookCar/respository/invoice/InvoiceDetailRepository.java
+++ backend/bookCar/src/main/java/com/freelance/bookCar/respository/invoice/InvoiceDetailRepository.java
@@ -2,11 +2,21 @@ package com.freelance.bookCar.respository.invoice;
 
 import com.freelance.bookCar.models.invoice.InvoiceDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface InvoiceDetailRepository extends JpaRepository<InvoiceDetail,Integer> {
-    List<InvoiceDetail> findByMonthAndHotel(int month);
-    List<InvoiceDetail> findByMonthAndTour(int month);
-    List<InvoiceDetail> findByMonthAndTourism(int month);
+
+
+    @Query(value = "SELECT id.* FROM invoice_detail id JOIN invoice i ON id.id_invoice = i.id WHERE id.id_hotel = :idHotel AND MONTH(i.invoice_date) = :month", nativeQuery = true)
+    List<InvoiceDetail> findByMonthAndHotel(@Param("month") int month, @Param("idHotel") int idHotel);
+
+    @Query(value = "SELECT id.* FROM invoice_detail id JOIN invoice i ON id.id_invoice = i.id WHERE id.id_tour = :idTour AND MONTH(i.invoice_date) = :month", nativeQuery = true)
+    List<InvoiceDetail> findByMonthAndTour(@Param("month") int month, @Param("idTour") int idTour);
+
+    @Query(value = "SELECT id.* FROM invoice_detail id JOIN invoice i ON id.id_invoice = i.id WHERE id.id_tourism = :idTourism AND MONTH(i.invoice_date) = :month", nativeQuery = true)
+    List<InvoiceDetail> findByMonthAndTourism(@Param("month") int month, @Param("idTourism") int idTourism);
+
 }
