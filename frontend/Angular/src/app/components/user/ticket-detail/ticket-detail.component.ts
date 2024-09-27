69e88e189a962c91b89cb609e4469eb3e67f4370
trungkien <145746252+allpedog@users.noreply.github.com>
2024-09-27 10:18:48 +0700

    hotel detail và tourism detail

--- frontend/Angular/src/app/components/user/ticket-detail/ticket-detail.component.ts
+++ frontend/Angular/src/app/components/user/ticket-detail/ticket-detail.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { GetTourismDetailResponse } from '../../../models/response/product/ticket/tourism/get-tourism-detail-response';
+import { ActivatedRoute } from '@angular/router';
+import { TourismService } from '../../../services/product/ticket/tourism/tourism.service';
 
 @Component({
   selector: 'app-ticket-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ticket-detail.component.html',
   styleUrl: './ticket-detail.component.css'
 })
 export class TicketDetailComponent {
 
+  @Input() location: string | null = null;
+  isExpanded = false; 
+  locationId: string | null = null;
+  locations: GetTourismDetailResponse[] = [];
+
+  toggleContent(event: Event) {
+    event.preventDefault(); 
+    this.isExpanded = !this.isExpanded;
+  }
+  
+
+  constructor(private route: ActivatedRoute,private tourismService : TourismService) { }
+
+  ngOnInit(): void {
+    console.log("tourismDetailComponent initialized");
+    this.route.paramMap.subscribe(params => {
+      this.locationId = params.get('id');
+      console.log("tourism "+this.locationId);
+      if (this.locationId) {
+        this.getTourismDetailById(parseInt(this.locationId));
+      }
+    });
+  }
+
+  getTourismDetailById(id: number) {
+    this.tourismService.getTourismDetailById(id).subscribe(response => {
+      console.log(response);
+      if (response) {
+        this.locations = [response];
+      } else {
+        console.log("Thất bại");
+      }
+    }, error => {
+      console.log("Error:", error);
+    });
+  }
 }
