40cd2171213eaa71659483d708f8b8b863cc2bfc
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-09-28 17:51:36 +0700

    add statisticyear

--- backend/bookCar/src/main/java/com/freelance/bookCar/dto/request/page/CreatePageRequest.java
+++ backend/bookCar/src/main/java/com/freelance/bookCar/dto/request/page/CreatePageRequest.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+
 public class CreatePageRequest {
     private Integer id;
     private String name;
--- backend/bookCar/src/main/java/com/freelance/bookCar/services/page/PageService.java
+++ backend/bookCar/src/main/java/com/freelance/bookCar/services/page/PageService.java
@@ -2,7 +2,9 @@ package com.freelance.bookCar.services.page;
 
 import com.freelance.bookCar.dto.request.page.CreatePageRequest;
 import com.freelance.bookCar.dto.response.page.CreatePageReponse;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface PageService {
     CreatePageReponse page(CreatePageRequest createPageRequest);
 }
