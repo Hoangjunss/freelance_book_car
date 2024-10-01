58df32203f15881e45c1f90dd4b6c8c51aeaad33
trungkien <145746252+allpedog@users.noreply.github.com>
2024-10-01 09:43:57 +0700

    kiên

--- frontend/Angular/src/app/components/user/navbar/navbar.component.ts
+++ frontend/Angular/src/app/components/user/navbar/navbar.component.ts
@@ -1,17 +1,70 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit} from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../../services/user/user.service';
-
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  CurrentUser: string | null = null;
+  mobileNavbarContent: string = '';
+  socialLinks: { icon: string, url: string }[] = [
+    { icon: 'fa-brands fa-facebook', url: '#' },
+    { icon: 'fa-brands fa-x', url: '#' },
+    { icon: 'fa-brands fa-youtube', url: '#' },
+    { icon: 'fa-brands fa-tiktok', url: '#' },
+    { icon: 'fa-brands fa-instagram', url: '#' },
+  ];
+  
+  navbarContent: { label: string, routerLink?: string, action?: () => void, isUser?: boolean }[] = [];
+  dataLoaded: boolean = false;
 
+  constructor(private userService: UserService, private router: Router, @Inject(PLATFORM_ID) private platformId: Object) { }
 
+  ngOnInit(): void {
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const user = localStorage.getItem('currentUser');
+      if (user) {
+        this.CurrentUser = JSON.parse(user).name; 
+      } else {
+        this.CurrentUser = null; 
+      }
+    }
+    
+    this.updateNavbarContent();
+    this.dataLoaded = true; 
+  }
+
+  updateNavbarContent(): void {
+    if (this.CurrentUser) {
+      this.navbarContent = [
+        { label: 'Logout', action: () => this.logout() },
+        { label: `${this.CurrentUser}`, isUser: true }
+      ];
+    } else {
+      this.navbarContent = [
+        { label: 'Sign Up', routerLink: '/auth/login' },
+        { label: 'My Account', isUser: false }
+      ];
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    localStorage.removeItem('token');
+    this.CurrentUser = null; 
+    this.updateNavbarContent();
+
+    // Navigate to login page
+    this.router.navigate(['/auth/login']);
+  }
 }
