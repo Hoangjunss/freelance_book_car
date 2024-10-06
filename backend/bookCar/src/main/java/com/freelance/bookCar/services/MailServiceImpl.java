c99a5e80f93b181e43409f9015189816f6465f9c
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2024-10-06 14:40:19 +0700

    khanh

--- backend/bookCar/src/main/java/com/freelance/bookCar/services/MailServiceImpl.java
+++ backend/bookCar/src/main/java/com/freelance/bookCar/services/MailServiceImpl.java
@@ -30,7 +30,7 @@ public class MailServiceImpl implements MailService {
             mimeMessageHelper.setSubject(mail.getMailSubject());
             mimeMessageHelper.setFrom(new InternetAddress(mail.getMailFrom()));
             mimeMessageHelper.setTo(mail.getMailTo());
-            mimeMessageHelper.setText(mail.getMailContent());
+            mimeMessageHelper.setText(mail.getMailContent(), true);
             javaMailSender.send(mimeMessageHelper.getMimeMessage());
         } catch (MessagingException e) {
             log.error("Failed to send mail", e);
